d8605405e3ef8e1cd79811d47a82cb3374e1f5ca
Korneliusz Rabczak <nikom1337@gmail.com>
2016-08-17 21:17:49 +0200

    adds maven project, basic dependencies and thymeleaf as a template engine, fixes #1

---
+++ src/main/java/com/thecookiezen/DockhandApplication.java
@@ -0,0 +1,12 @@
+package com.thecookiezen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockhandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockhandApplication.class, args);
+	}
+}
